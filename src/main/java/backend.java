40bb5ab4cf12b486af9950f071c5c433e0b339ed
danyzaher <dany0.zaher@etu.u-pec.fr>
2021-02-06 13:00:55 +0100

    :construction: add logger instead of systemoutprint

---
+++ src/main/java/backend.java
@@ -0,0 +1,9 @@
+import java.util.logging.Logger;
+
+public class backend {
+    private final static Logger logger = Logger.getLogger("backend");
+    public static void main(String[] args) {
+        logger.info("Running backend service.");;
+    }
+}
+
