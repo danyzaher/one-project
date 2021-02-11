a9ce5bd48d24f3fea9193046ee47e335f24e2980
danyzaher <dany0.zaher@etu.u-pec.fr>
2021-02-11 21:18:44 +0100

    update workspace

--- src/main/java/backend.java
+++ src/main/java/backend.java
@@ -1,20 +1,18 @@
-
 import ch.qos.logback.classic.Logger;
 import org.apache.commons.cli.*;
 import org.slf4j.LoggerFactory;
 
-import javax.swing.*;
-
-
 public class backend {
     private final static Logger logger = (Logger) LoggerFactory.getLogger(backend.class.getName());
     public static void main(String[] args) throws ParseException {
-        JFrame frame = new App();
         final Options options = new Options();
         final Option test = Option.builder().longOpt("test").build();
         options.addOption(test);
+        final Option Name = Option.builder().longOpt("Name").hasArgs().argName("Name").build();
+        options.addOption(Name);
         final CommandLineParser parser = new DefaultParser();
         final CommandLine cl = parser.parse(options,args);
         logger.info("Running service (test = {})", cl.hasOption("test"));
+        logger.info("The client is online. The name of the client is {}",cl.getOptionValue("Name"));
     }
 }
