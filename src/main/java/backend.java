e664a175ee03a1e8d71191d221cf85b9ff7a5bf9
danyzaher <dany0.zaher@etu.u-pec.fr>
2021-02-11 19:03:37 +0100

    update workspace

--- src/main/java/backend.java
+++ src/main/java/backend.java
@@ -1,9 +1,11 @@
+
+import javax.swing.*;
 import java.util.logging.Logger;
 
 public class backend {
     private final static Logger logger = Logger.getLogger("backend");
     public static void main(String[] args) {
-        logger.info("Running backend service.");;
+        JFrame frame = new App();
+        logger.info("Running service.");
     }
 }
-
