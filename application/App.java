9c233ecb9a15f2be734083c833f9c93592505a24
dany zaher <dany0.zaher@etu.u-pec.fr>
2021-02-22 15:13:06 +0100

    :lipstick: work on application style

--- application/App.java
+++ application/App.java
@@ -9,15 +9,28 @@ public class App extends JFrame {
         Image icon = Toolkit.getDefaultToolkit().getImage("C:\\Users\\Dany\\Documents\\one-project\\files\\logo.png");
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         JButton button = new JButton("test");
-        JPanel pane = new JPanel();
-        pane.add(button);
-        setContentPane(pane);
-        setSize(500,500);
+        setSize(800,700);
         setDefaultLookAndFeelDecorated(true);
         setBackground(Color.PINK);
-        setBounds(500,250,500,250);
         setIconImage(icon);
-        setVisible(true);
         setLayout(null);
+        setLocationRelativeTo(null);
+        setResizable(false);
+        Panneau pane = new Panneau();
+        pane.add(button);
+        setContentPane(pane);
     }
+    public static void main(String[] arg) {
+        App fenetre = new App();
+        fenetre.setVisible(true);
+    }
+}
+class Panneau extends JPanel {
+    public void paintComponent(Graphics g) {                
+        int x1 = this.getWidth()/4;
+        int y1 = this.getHeight()/4;                      
+        g.drawRect(x1, y1, this.getWidth()/2, this.getHeight()/2);
+        g.drawLine(x1+50, y1,x1+50,y1+106);
+        
+  }       
 }
