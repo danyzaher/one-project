0e7b46bdd26e696f620f53969b02f67c96766199
dany zaher <dany0.zaher@etu.u-pec.fr>
2021-02-22 16:06:40 +0100

    :construction: one floor made

--- application/App.java
+++ application/App.java
@@ -11,11 +11,11 @@ public class App extends JFrame {
         JButton button = new JButton("test");
         setSize(800,700);
         setDefaultLookAndFeelDecorated(true);
-        setBackground(Color.PINK);
+        setBackground(new Color(899999));
         setIconImage(icon);
         setLayout(null);
         setLocationRelativeTo(null);
-        setResizable(false);
+        setResizable(true);
         Panneau pane = new Panneau();
         pane.add(button);
         setContentPane(pane);
@@ -26,11 +26,25 @@ public class App extends JFrame {
     }
 }
 class Panneau extends JPanel {
-    public void paintComponent(Graphics g) {                
-        int x1 = this.getWidth()/4;
-        int y1 = this.getHeight()/4;                      
-        g.drawRect(x1, y1, this.getWidth()/2, this.getHeight()/2);
-        g.drawLine(x1+50, y1,x1+50,y1+106);
+    public void paintComponent(Graphics g) {       
+        int x2 = this.getWidth()/2;
+        int y2 = this.getHeight()/2;
+
+        int x3 = this.getWidth()/3;
+        int y3 = this.getHeight()/3;
+
+        int x4 = this.getWidth()/4;
+        int y4 = this.getHeight()/4;
         
+        int x8 = this.getWidth()/8;
+        int y8 = this.getHeight()/8;   
+        Font font = new Font("Courier", Font.BOLD,20);
+        g.setFont(font);
+        g.drawString("Choisissez un étage",10,30);           
+        g.drawRect(x4, y4, x2, y2);
+        g.drawRect(x4,y4, x8, y8);
+        g.drawRect(x4,y2, x8,y4);
+        g.drawRect(x2, y4,x4,y4);
+        g.drawRect(x2,y2+30, x4,y4-30);
   }       
 }
