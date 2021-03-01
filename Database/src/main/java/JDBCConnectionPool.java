5adfced6bd61704cf24b320c971667beef8db264
Julien Cannoux <juliencannoux@etu.u-pec.fr>
2021-03-01 19:25:55 +0100

    JDBC Connection Pool update test

---
+++ Database/src/main/java/JDBCConnectionPool.java
@@ -0,0 +1,63 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+
+public class JDBCConnectionPool {
+
+    private ArrayList<Connection> collection;
+
+    private Connectioninfo connectioninfo;
+
+    public JDBCConnectionPool(int nboneco) {
+
+        this.collection = new ArrayList<>();
+        this.connectioninfo = new Connectioninfo();
+
+        addConnections(nboneco);
+    }
+
+
+    public void addConnections(int nboneco)
+    {
+
+        Connection oneco;
+
+        for (int i = 0; i < nboneco; i++) {
+
+            try {Class.forName(connectioninfo.getDriver());
+
+                oneco = DriverManager.getConnection(connectioninfo.getUrl(),connectioninfo.getUser(), connectioninfo.getPassword());
+
+                collection.add(oneco);
+
+            } catch (SQLException | ClassNotFoundException throwables) {
+                throwables.printStackTrace();
+            }
+        }
+    }
+
+    public Connection getConnection()
+    {
+        Connection oneco = collection.get(0);
+        collection.remove(0);
+        return oneco;
+    }
+
+
+    public void endConnections()
+    {
+
+        for (int i=0; i <= collection.size(); i++) {
+            try {
+                getConnection().close();
+            } catch (SQLException throwables) {
+                throwables.printStackTrace();
+            }
+        }
+    }
+    public void setConnection(Connection oneco) {
+        collection.add(oneco);
+    }
+}
