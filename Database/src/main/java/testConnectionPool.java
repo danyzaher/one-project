9db7cba31cf2f6aa0ac108b9664265d23e181441
danyzhr <danyzhr@live.fr>
2021-03-06 19:42:50 +0100

    :zap: add crud options

--- Database/src/main/java/testConnectionPool.java
+++ Database/src/main/java/testConnectionPool.java
@@ -14,8 +14,13 @@ public class testConnectionPool {
         smt.executeUpdate(sql);
     }
 
-    public static void eraseElement(Connection c, String Table, String idcolumn, int id) throws SQLException {
+    public static void eraseElement(Connection c, String Table, String idcolumn, String id) throws SQLException {
         String sql = "DELETE FROM \"" + Table + "\" WHERE " + idcolumn + " = " + id + ";";
+        Statement smt = c.createStatement();
+        smt.executeUpdate(sql);
+    }
+    public static void deleteElement(Connection c, String Table, String idcolumn) throws SQLException {
+        String sql = "delete from \"" + Table + "\" where " + idcolumn + " = (select max(" + idcolumn + ") from \"" + Table + "\");";
         Statement smt = c.createStatement();
         smt.executeUpdate(sql);
     }
@@ -34,33 +39,83 @@ public class testConnectionPool {
         final Options options = new Options();
         final Option maxConnection = Option.builder().longOpt("maxConnection").hasArg().build();
         options.addOption(maxConnection);
+        final Option show = Option.builder().longOpt("show").hasArg().build();
+        options.addOption(show);
+        final Option create = Option.builder().longOpt("create").hasArg().build();
+        options.addOption(create);
         final Option timeOut = Option.builder().longOpt("timeOut").hasArg().build();
         options.addOption(timeOut);
+        final Option delete = Option.builder().longOpt("delete").hasArg().build();
+        options.addOption(delete);
         final CommandLineParser parser = new DefaultParser();
         final CommandLine commandLine = parser.parse(options, args);
         int iMaxConnection = 10;
         int itimeOut = 5000;
+        boolean iShow = false, iCreate = false, iDelete = false;
         if (commandLine.hasOption("maxConnection"))
             iMaxConnection = Integer.parseInt(commandLine.getOptionValue("maxConnection"));
         if (commandLine.hasOption("timeOut"))
             itimeOut = Integer.parseInt(commandLine.getOptionValue("timeOut"))*1000;
+        iShow = commandLine.hasOption("show");
+        iCreate = commandLine.hasOption("create");
+        iDelete = commandLine.hasOption("delete");
         if (iMaxConnection>0) {
             Datasource source = new Datasource(iMaxConnection);
             int i = 0;
-            while (!source.isEmpty()) {
-                logger.info("Number of connections: " + source.size());
-                connectionManager.add(i, source.getConnection());
-                showElement(connectionManager.get(i), "distance", "id_distance", "risque");
-                sleep(itimeOut);
-                i++;
+            if (iShow) {
+                while (!source.isEmpty()) {
+                    logger.info("Number of connections: " + source.size());
+                    connectionManager.add(i, source.getConnection());
+                    showElement(connectionManager.get(i), "distance", "id_distance", "risque");
+                    sleep(itimeOut);
+                    i++;
+                }
+                logger.info("no more connections");
+                logger.info("retrieve connection pool");
+                while (!connectionManager.isEmpty()) {
+                    logger.info("Number of connections: " + source.size());
+                    source.setConnection(connectionManager.get(0));
+                    connectionManager.remove(0);
+                    sleep(itimeOut);
+                }
             }
-            logger.info("no more connections");
-            logger.info("retrieve connection pool");
-            while (!connectionManager.isEmpty()) {
-                logger.info("Number of connections: " + source.size());
-                source.setConnection(connectionManager.get(0));
-                connectionManager.remove(0);
-                sleep(itimeOut);
+            if (iCreate) {
+                while (!source.isEmpty()) {
+                    logger.info("Number of connections: " + source.size());
+                    connectionManager.add(i, source.getConnection());
+                    logger.info("adding element");
+                    addElement(connectionManager.get(i), "distance", "risque", commandLine.getOptionValue("create"));
+                    showElement(connectionManager.get(i), "distance", "id_distance", "risque");
+                    sleep(itimeOut);
+                    i++;
+                }
+                logger.info("no more connections");
+                logger.info("retrieve connection pool");
+                while (!connectionManager.isEmpty()) {
+                    logger.info("Number of connections: " + source.size());
+                    source.setConnection(connectionManager.get(0));
+                    connectionManager.remove(0);
+                    sleep(itimeOut);
+                }
+            }
+            if (iDelete) {
+                while (!source.isEmpty()) {
+                    logger.info("Number of connections: " + source.size());
+                    connectionManager.add(i, source.getConnection());
+                    logger.info("deleting element");
+                    deleteElement(connectionManager.get(i), "distance", "id_distance");
+                    showElement(connectionManager.get(i), "distance", "id_distance", "risque");
+                    sleep(itimeOut);
+                    i++;
+                }
+                logger.info("no more connections");
+                logger.info("retrieve connection pool");
+                while (!connectionManager.isEmpty()) {
+                    logger.info("Number of connections: " + source.size());
+                    source.setConnection(connectionManager.get(0));
+                    connectionManager.remove(0);
+                    sleep(itimeOut);
+                }
             }
             source.closeConnections();
         } else {
