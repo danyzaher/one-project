558ea23c8f07a5d865a21d1c8fef95dc307048d7
YassirChirawi <78093690+YassirChirawi@users.noreply.github.com>
2021-03-10 12:31:52 +0100

    script

--- Database/scripts/Linux/database-launch.sh
+++ Database/scripts/Linux/database-launch.sh
@@ -1,3 +1,17 @@
 cd ../../target/
-java -jar database-1.0-SNAPSHOT-jar-with-dependencies.jar -maxConnection=10 -timeOut=1 -create="fromage"
+read -p 'add or delete?: ' c
+
+if [ "$c" = "add" ]
+then
+  read -p 'Entrez maxConnection: '  b
+  read -p 'Entrez timeOut : ' t
+  read -p 'Entrez ce que vous voulez ajouter : ' nom
+  java -jar database-1.0-SNAPSHOT-jar-with-dependencies.jar -maxConnection=$b -timeOut=$t -create=$nom
+
+else
+  read -p 'Entrez maxConnection: '  b
+  read -p 'Entrez timeOut : ' t
+  read -p 'Entrez ce que vous voulez effacer : ' nom
+  java -jar database-1.0-SNAPSHOT-jar-with-dependencies.jar -maxConnection=$b -timeOut=$t -delete=$nom
+fi
 cd ..
