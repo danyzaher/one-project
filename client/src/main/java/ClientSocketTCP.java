79c99890c9de8b436c6437cf38e69621fbe9d47f
MaximeFritel <maxime.fritel@etu.u-pec.fr>
2021-03-17 16:52:40 +0100

    Client-serveur TCP localhost

---
+++ client/src/main/java/ClientSocketTCP.java
@@ -0,0 +1,45 @@
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.*;
+import java.net.*;
+
+public class ClientSocketTCP {
+
+    protected static Logger clientLog  = LoggerFactory.getLogger("clientSocketTCP");
+
+    public static void main(String[] args) throws IOException {
+        String hostName = "localhost";
+        String nom = "julien";
+        Socket sock = null;
+        PrintWriter out = null;
+        ObjectInputStream in = null;
+        try {
+            sock = new Socket(hostName, 3333);
+            out = new  PrintWriter(sock.getOutputStream(), true);
+            in = new ObjectInputStream(sock.getInputStream());
+        } catch(UnknownHostException e) {
+            System.err.println("Host injoignable : " + hostName);
+            System.exit(1);
+        } catch(IOException ee) {
+            System.err.println("Connexion impossible avec : " + hostName);
+            System.exit(1);
+        }
+        out.println(nom);
+        try {
+            Object recu = in.readObject();
+            if (recu == null) System.err.println("Erreur");
+            else {
+                PU pu = (PU)recu;
+                System.out.println("Serveur->Client : " + pu);
+            }
+        } catch(ClassNotFoundException e) {
+            System.err.println("Classe inconnue : " + hostName);
+            System.exit(1);
+        }
+        out.close();
+        in.close();
+        sock.close();
+    }
+
+}
---
+++ client/src/main/java/ServerSocketTCP.java
@@ -0,0 +1,40 @@
+import java.net.*;
+import java.io.*;
+
+public class ServerSocketTCP {
+
+    public static void main(String[] args) {
+	PU [] tab = {
+	    new PU("dany", "POO", 18),
+	    new PU("maxime", "math", 18),
+	    new PU("julien", "anglais", 11)};
+
+	ServerSocket server = null;
+	try {
+	    server = new ServerSocket(3333);
+	    while (true) {
+		Socket sock = server.accept();
+		System.out.println("Connected");
+		ObjectOutputStream out = new ObjectOutputStream(sock.getOutputStream());
+		BufferedReader in = new BufferedReader(new InputStreamReader(sock.getInputStream()));
+		String recu;
+		while ((recu = in.readLine()) != null) {
+		    System.out.println("recu : " + recu);
+		    String nom = recu.trim();
+		    for(int i = 0; i < tab.length; i++)
+			if (tab[i].getNom().equals(nom)) {
+				out.writeObject(tab[i]);
+				break;
+				}	
+		}
+		out.close();
+		sock.close();
+		in.close();
+	    }
+	} catch(IOException e) {
+		try {
+		    server.close();
+		} catch(IOException ee) {} 
+	}
+    }
+} 
