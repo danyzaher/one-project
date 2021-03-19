bcbdd41d32b86d42cb616a0d5b250a831229aa69
Julien Cannoux <juliencannoux@etu.u-pec.fr>
2021-03-19 13:28:32 +0100

    reload client part

--- client/src/main/java/Model.java
+++ client/src/main/java/Model.java
@@ -5,23 +5,22 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
-
 public class Model {
 
     private final static Logger logger = (Logger) LoggerFactory.getLogger(Model.class.getName());
 
-    public String showElement(Connection c , String Table) throws SQLException {
+    public String showElement(Connection c , String nomElement ) throws SQLException {
 
-        String sql = "select * from \"" + Table + "\"";
+        String sql = "select * from produit where nom="+nomElement+";";
         Statement smt = c.createStatement();
         ResultSet rs = smt.executeQuery(sql);
         String result = null;
         while (rs.next()) {
-            result = result + "/n" + rs.getArray(Table);
+
+            result = result + "/n" + rs.getArray(nomElement);
         }
         return result;
     }
    // System.out.println("Show is done");
-
 }
 
--- client/src/main/java/choice.java
+++ client/src/main/java/choice.java
@@ -7,20 +7,14 @@ public class choice {
         modeltest = new Model();
     }
     public void menu(){
-        System.out.println("tape --read : ");
-        Scanner scanner= new Scanner(System.in);
-        
+
     }
     
     public String choixUtilisateur(Scanner scanner){
         
-        String choix = scanner.nextLine();
-        if(choix == "--read"){
-            showElement(scanner);
+
+
+
         }
     }
-
-    private void showElement(Scanner scanner) {
-    }
-}
 
