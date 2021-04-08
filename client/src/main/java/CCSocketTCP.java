cef28d5c2dae059f82b83ea29b1d91af236f9a16
Julien Cannoux <juliencannoux@etu.u-pec.fr>
2021-04-09 01:21:48 +0200

    please

--- client/src/main/java/CCSocketTCP.java
+++ client/src/main/java/CCSocketTCP.java
@@ -1,5 +1,3 @@
-
-
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ArrayNode;
 import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
--- server/scripts/launch.sh
+++ server/scripts/launch.sh
@@ -1,2 +1,2 @@
 cd ..
-exec  java -jar target/server-1.0-SNAPSHOT-jar-with-dependencies.jar
+java -jar target/server-1.0-SNAPSHOT-jar-with-dependencies.jar
