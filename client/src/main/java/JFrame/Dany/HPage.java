cf4c760b581e95086921e10a69321c2fecb7c57b
MaximeFritel <maxime.fritel@etu.u-pec.fr>
2021-04-12 20:07:20 +0200

    HPage jButton

--- client/src/main/java/JFrame/Dany/HPage.java
+++ client/src/main/java/JFrame/Dany/HPage.java
@@ -1,27 +1,55 @@
 package JFrame.Dany;
 
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
 
-public class HPage extends JFrame implements ActionListener{
-    private JButton confirmerButton;
-    
+public class HPage extends JFrame implements ItemListener{
 
-    public HPage() {
-        this.setVisible(true);
+    JComboBox combobox;
+    JLabel l1, l2;
+    public HPage(){
         setTitle("page d'accueil");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLayout(new FlowLayout());
+        setSize(400, 200);
+        setVisible(true);
+
         String s1[] = { "Fritel Inc", "FloraFlore" };
-        JComboBox jComboBox = null; 
-        jComboBox.setModel(new DefaultComboBoxModel(s1));
-        add(jComboBox);
+
+        combobox = new JComboBox(s1);
+        combobox.addItemListener(this);
+
+        l1 = new JLabel("choisissez une société ");
+        l2 = new JLabel("[Fritel Inc]");
+
+        l2.setForeground(Color.blue);
+
+        // créer un nouveau panneau
+        JPanel p = new JPanel();
+        // ajouter combobox et labels au panneau
+        p.add(l1);
+        p.add(combobox);
+        p.add(l2);
+        // ajouter le panneau au frame
+        add(p);
+        JButton jButton = new JButton("Confirmer");
+        add(jButton);
+        jButton.setBounds(200,100,50,50);
     }
     public static void main(String[] args){
-        HPage hPage = new HPage();
+         HPage h = new HPage();
+    }
+    public void itemStateChanged(ItemEvent e)
+    {
+        // si l'état du combobox est modifiée
+        if (e.getSource() == combobox) {
+
+            l2.setText(" ["+combobox.getSelectedItem()+"]");
+        }
     }
 
-    @Override
-    public void actionPerformed(ActionEvent e) {
-
-    }
 }
