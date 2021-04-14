1848b8a5698e32748e1098761f1acd77d77b8b84
Julien Cannoux <juliencannoux@etu.u-pec.fr>
2021-04-14 15:55:59 +0200

    Creation of temperature, store and windows JSliders

--- client/src/main/java/JFrame/julien/OptionChroma.java
+++ client/src/main/java/JFrame/julien/OptionChroma.java
@@ -8,5 +8,13 @@ public class OptionChroma extends JFrame{
     JSlider Store;
     JSlider Fenetre;
 
+    public OptionChroma() {
+        setLayout(new FlowLayout());
+        temperature = new JSlider(JSlider.HORIZONTAL, 0,30, 19);
+        temperature.setMajorTickSpacing(1);
+        temperature.setPaintTicks(true);
+        add(temperature);
 
+        //ATemp = new JLabel("Valeur actuel : 19°C");
+    }
 }
