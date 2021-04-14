dbf3bec1bd20f8284114402584944d126af8724b
Julien Cannoux <juliencannoux@etu.u-pec.fr>
2021-04-14 16:42:38 +0200

    Add Label for the temperature and light intensity

--- client/src/main/java/JFrame/julien/OptionChroma.java
+++ client/src/main/java/JFrame/julien/OptionChroma.java
@@ -4,7 +4,9 @@ import java.awt.*;
 import javax.swing.event.*;
 
 public class OptionChroma extends JFrame{
+    /*Lower part of the frame*/
 
+    //Sliders and Labels
     JSlider temperature;
     JSlider store;
     JSlider fenetre;
@@ -13,10 +15,25 @@ public class OptionChroma extends JFrame{
     JLabel AStore;
     JLabel AFen;
 
+    /*Upper part of thr frame*/
+    JLabel tempext;
+    JLabel tempint;
+    JLabel eclairage;
+
+
     public OptionChroma() {
 
         setLayout(new FlowLayout());
-        //Temperature part
+        tempext = new JLabel("Temperature à l'exterieur : 5°C");
+        tempint = new JLabel("Temperature dans la salle : 20°C");
+        eclairage = new JLabel("Taux de luminosité dans la salle : 50%");
+        add(tempext);
+        add(tempint);
+        add(eclairage);
+
+
+
+        //Temperature lower part
 
         temperature = new JSlider(JSlider.HORIZONTAL, 0,30, 19);
         temperature.setMajorTickSpacing(1);
@@ -28,7 +45,7 @@ public class OptionChroma extends JFrame{
         eventTemp et = new eventTemp();
         temperature.addChangeListener(et);
 
-        //Store Part
+        //Store lower Part
         store = new JSlider(JSlider.HORIZONTAL, 0,5, 0);
         store.setMajorTickSpacing(1);
         store.setPaintTicks(true);
@@ -39,7 +56,7 @@ public class OptionChroma extends JFrame{
         eventStor es = new eventStor();
         store.addChangeListener(es);
 
-        //Windows Part
+        //Windows lower Part
         fenetre = new JSlider(JSlider.HORIZONTAL, 0,5, 0);
         fenetre.setMajorTickSpacing(1);
         fenetre.setPaintTicks(true);
@@ -57,8 +74,9 @@ public class OptionChroma extends JFrame{
 
         @Override
         public void stateChanged(ChangeEvent e) {
+
             int valeurfen = fenetre.getValue();
-            AFen.setText("Opacité actuelle de la fenetre : "+valeurfen);
+            AFen.setText("Opacité de la fenetre selectionnée souhaité  : "+valeurfen);
         }
     }
     public class eventStor implements ChangeListener{
@@ -66,7 +84,7 @@ public class OptionChroma extends JFrame{
         @Override
         public void stateChanged(ChangeEvent e) {
             int valeurstor = store.getValue();
-            AStore.setText("Hauteur actuelle du store : "+ valeurstor);
+            AStore.setText("Hauteur du store selectionné souhaité : "+ valeurstor);
         }
     }
     public class eventTemp implements ChangeListener {
@@ -75,7 +93,7 @@ public class OptionChroma extends JFrame{
         public void stateChanged(ChangeEvent et) {
 
             int valeurtemp = temperature.getValue();
-            ATemp.setText("Temperature actuelle dans la salle : "+ valeurtemp + "° C");
+            ATemp.setText("Temperature dans la salle souhaitée : "+ valeurtemp + "° C");
 
         }
 
@@ -83,11 +101,12 @@ public class OptionChroma extends JFrame{
 
     }
 
+
     public static void main(String[] args) {
 
         OptionChroma opchro = new OptionChroma();
         opchro.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        opchro.setSize(500,200);
+        opchro.setSize(300,400);
         opchro.setVisible(true);
         opchro.setTitle("Parametres des Options ElectroChromatiques ");
 
