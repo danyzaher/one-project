0f3421d60cba7160a9ff85acabcd2f0e416b4757
Julien Cannoux <juliencannoux@etu.u-pec.fr>
2021-04-14 15:51:33 +0200

    Creation frame Electro chromatic option

---
+++ client/src/main/java/JFrame/julien/OptionChroma.java
@@ -0,0 +1,12 @@
+package JFrame.julien;
+import javax.swing.*;
+import java.awt.*;
+import javax.swing.event.*;
+
+public class OptionChroma extends JFrame{
+    JSlider temperature;
+    JSlider Store;
+    JSlider Fenetre;
+
+
+}
