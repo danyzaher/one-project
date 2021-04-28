26b75dfac5063ac29e22489cb462867d880c49ce
danyzhr <danyzhr@live.fr>
2021-04-28 19:48:37 +0200

    make list of ids for offers

--- client/src/main/java/jframe/danyter/OfferSelected.java
+++ client/src/main/java/jframe/danyter/OfferSelected.java
@@ -1,15 +1,36 @@
 package jframe.danyter;
 
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class OfferSelected extends JFrame{
+public class OfferSelected extends JFrame implements ActionListener {
+    JPanel bigpan = new JPanel();
     JLabel description = new JLabel("Batiment D 15202");
     JPanel right = new JPanel();
     JPanel left = new JPanel();
-    JLabel finalprice;
     JButton rent = new JButton("Louer");
+    jframe.danyter.OneOffer oneOffer;
+    public OfferSelected(jframe.danyter.OneOffer on) {
+        oneOffer = on;
+        setTitle("Détails de l'offre");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(800, 300);
+        setResizable(false);
+        setLayout(new FlowLayout());
+        right.setLayout(new BoxLayout(right, BoxLayout.LINE_AXIS));
+        left.setLayout(new BoxLayout(left, BoxLayout.LINE_AXIS));
+        bigpan.add(right);
+        bigpan.add(left);
+        bigpan.setLayout(new BoxLayout(bigpan, BoxLayout.PAGE_AXIS));
+        this.add(bigpan);
+    }
 
-    public OfferSelected() {
-
+    @Override
+    public void actionPerformed(ActionEvent actionEvent) {
+        if (actionEvent.getSource() == rent) {
+            jframe.danyter.Success s = new jframe.danyter.Success();
+        }
     }
 }
--- client/src/main/java/jframe/danyter/OneOffer.java
+++ client/src/main/java/jframe/danyter/OneOffer.java
@@ -7,21 +7,37 @@ public class OneOffer extends JPanel {
     JPanel right = new JPanel();
     JLabel title;
     JLabel where;
-    JLabel potentialprice;
+    JLabel pri;
     JButton button = new JButton("choisir");
 
     public OneOffer (String ti, String wh, String price) {
         title = new JLabel(ti);
         where = new JLabel(wh);
-        potentialprice = new JLabel(price);
+        pri = new JLabel(price);
 
         left.setLayout(new BoxLayout(left,BoxLayout.LINE_AXIS));
         right.setLayout(new BoxLayout(right,BoxLayout.LINE_AXIS));
         left.add(title);
         left.add(where);
-        left.add(potentialprice);
+        left.add(pri);
         right.add(button);
         this.setLayout(new BoxLayout(this,BoxLayout.PAGE_AXIS));
         this.add(left);this.add(right);
+    }
+
+    public JButton getButton() {
+        return button;
+    }
+
+    public JLabel getPrice() {
+        return pri;
+    }
+
+    public JLabel getTitle() {
+        return title;
+    }
+
+    public JLabel getWhere() {
+        return where;
     }
 }
