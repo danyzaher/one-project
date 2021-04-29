0ec544798630f8306e93c16bda89fb08e21b11c4
root <root@pc-moscatelli>
2021-04-29 09:31:29 +0200

    :wrench: finishing offers builder

--- client/src/main/java/jframe/danyter/OneOffer.java
+++ client/src/main/java/jframe/danyter/OneOffer.java
@@ -6,19 +6,18 @@ public class OneOffer extends JPanel {
     JPanel left = new JPanel();
     JPanel right = new JPanel();
     JLabel title;
-    JLabel where;
     JLabel pri;
     JButton button = new JButton("choisir");
 
-    public OneOffer (String ti, String wh, String price) {
+    public OneOffer (String ti,String price) {
         title = new JLabel(ti);
-        where = new JLabel(wh);
+    
         pri = new JLabel(price);
 
         left.setLayout(new BoxLayout(left,BoxLayout.LINE_AXIS));
         right.setLayout(new BoxLayout(right,BoxLayout.LINE_AXIS));
         left.add(title);
-        left.add(where);
+
         left.add(pri);
         right.add(button);
         this.setLayout(new BoxLayout(this,BoxLayout.PAGE_AXIS));
@@ -35,9 +34,5 @@ public class OneOffer extends JPanel {
 
     public JLabel getTitle() {
         return title;
-    }
-
-    public JLabel getWhere() {
-        return where;
     }
 }
