3a4280fa9f190b3ef98b85838a59470d2b971852
Julien Cannoux <juliencannoux@etu.u-pec.fr>
2021-05-01 11:42:41 +0200

    update of the AutoModeElectro

--- server/src/main/java/AutoModeElectro.java
+++ server/src/main/java/AutoModeElectro.java
@@ -2,4 +2,20 @@
 
 public class AutoModeElectro {
 
+    Datasource dataSource;
+
+    public AutoModeElectro(Datasource dataSource) {
+
+        this.dataSource = dataSource;
+
+    }
+
+    public void BrainElectroChroma() {
+        while (true) {
+            if (dataSource.size() > 0) {
+                ConnectionCrud C = new ConnectionCrud();
+                C.setC(Datasource.getConnection());
+            }
+        }
+    }
 }
