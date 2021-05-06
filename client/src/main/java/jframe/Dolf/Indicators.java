35e06ea0567ee31bef231833ab0ad40f637fae9b
Dolf <fandiodolf@gmail.com>
2021-05-06 18:06:36 +0200

    modifications

--- client/src/main/java/jframe/Dolf/Indicators.java
+++ client/src/main/java/jframe/Dolf/Indicators.java
@@ -2,24 +2,30 @@ package jframe.Dolf;
 
 import jframe.HPageCompany;
 import jframe.maxime.button.GoBackButton;
+import jframe.maxime.button.Sensor;
+import socket.CCSocketTCPbis;
 
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
 
-public class Indicators extends JFrame implements ActionListener {
+public class Indicators<ind_occ> extends JFrame implements ActionListener {
     private JPanel panel1, panel2,panel3,panel4,panel5,panel6;
+    private String ind_occ;
      public Indicators(){
+         JButton SensorsP;
+
          //super(" ");
-         super.setFont(new Font(Font.SERIF,Font.BOLD,30));
+         super.setFont(new Font(Font.SERIF,Font.BOLD,20));
          this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-         this.setSize(600, 300);
+         this.setSize(800, 700);
          this.setLocationRelativeTo(null);
          // this.getContentPane().add(new JButton(),BorderLayout.CENTER);
          panel1=new JPanel();
          //panel1.setBackground(Color.WHITE);
-         panel1.setPreferredSize(new Dimension(400,300));
+         panel1.setPreferredSize(new Dimension(300,200));
          panel3=new JPanel();
          panel3.setPreferredSize(new Dimension(150, 150));
          panel4= new JPanel();
@@ -30,31 +36,33 @@ public class Indicators extends JFrame implements ActionListener {
          panel6.setPreferredSize(new Dimension(150, 150));
 
          panel2 =new JPanel();
-         //panel2.setBackground(Color.WHITE);
-         // panel3.setBackground(Color.CYAN);
+         //panel2.setBackground(Color.);
+         // panel3.setBackground(new Color(128, 152, 207));
          //panel4.setBackground(Color.CYAN);
 
-         this.add(panel1, BorderLayout.CENTER);
+         this.add(panel1, BorderLayout.SOUTH);
          this.add(panel2, BorderLayout.EAST);
-         this.add(panel3,BorderLayout.EAST);
+         this.add(panel3,BorderLayout.CENTER);
          this.add(panel4,BorderLayout.PAGE_START);
-         this.add(panel5,BorderLayout.PAGE_END);
+         //this.add(panel5,BorderLayout.PAGE_END);
          this.add(panel6,BorderLayout.WEST);
 
 
-         JLabel label= new JLabel(" My Indicators ");
-         label.setBackground(new Color(128, 152, 207));
+
+         JLabel label= new JLabel(" Mes Indicateurs ");
+         label.setFont(new Font(Font.SERIF,Font.BOLD,50));
          panel4.add(label);
-         JButton ind_occ = new JButton("Room occupation Rate");
+         JButton ind_occ = new JButton("Taux d'occupation");
          panel1.add(ind_occ);
          ind_occ.addActionListener(new Room_occupation_Rate());
-         JButton sensors= new JButton(" sensor's number");
-         panel6.add(sensors);
-         sensors.addActionListener(new Ind_Sensors_number());
-         JButton water= new JButton(" water_s_consumption");
+
+         SensorsP = new JButton(" nombre de capteurs");
+         SensorsP.addActionListener(new Ind_Sensors_number());
+         panel6.add(SensorsP);
+         JButton water= new JButton(" taux de consommation d'eau");
          panel3.add(water);
         water.addActionListener(new water_s_consumption());
-         JButton elect= new JButton(" electricity consumption");
+         JButton elect= new JButton("taux de consommation d'electricité");
          panel2.add(elect);
        elect.addActionListener(new electricity_consumption());
          JButton back = new JButton("Back");
@@ -74,11 +82,15 @@ public class Indicators extends JFrame implements ActionListener {
 
     @Override
     public void actionPerformed(ActionEvent e) {
-
+    if (e.getSource().equals(ind_occ)){
+        ArrayList<String> al = new ArrayList<>();
+       al.add("show");
+        al.add("room number");
+        CCSocketTCPbis client = new CCSocketTCPbis(al);
+        ArrayList<String> strings = client.result;
+    }
     }
 
-    public void newGoBack(HPageCompany hPageCompany) {
-    }
     public void newGoBack(JFrame j){
         GoBackButton goBackbutton = new GoBackButton(this, j);
         add(goBackbutton);
